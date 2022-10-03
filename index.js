5ed37cd92891bdc7c8b09b9e553952d9ecdf3a76
abdullah43577 <100965547+abdullah43577@users.noreply.github.com>
2022-10-03 09:50:25 +0100

    optimized for redundancies in code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dateEl = document.querySelector("#date");
 const amountEl = document.querySelector("#amount");
 const inputBtn = document.querySelector("#input-btn");
 const tableBody = document.querySelector("#t-body");
+const message = document.getElementById("msg");
 // getting the item from localStorage and passing in the parse method to parse it back to an object or array
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myTable"));
 
@@ -16,7 +17,7 @@ if (leadsFromLocalStorage) {
 function deleteRow(event) {
   // accessing the button id itself gives the same id of the row itself, calling this gives row 1
   let row = event.target.id;
-  //accessing the the last element of the row 1
+  //accessing the the last element of the selected row
   let index = row[row.length - 1];
   // deletes the row
   myTable.splice(index, 1);
@@ -34,7 +35,9 @@ function render(table) {
     for (let j = 0; j < table[i].length; j++) {
       tableItems += `<td class= "cell${[j]}">${table[i][j]}</td> `;
     }
+    //delete row button implementation
     tableItems += `<td><span onclick="deleteRow(event)" id="row${i}">X</span></td>`;
+    // nesting all table data's inside the table row
     tableHTML += `<tr> ${tableItems} </tr>`;
     //empty table items at the end of the loop to avoid congestion of the previous tableItems stored in the variable
     tableItems = "";
@@ -43,7 +46,7 @@ function render(table) {
   tableBody.innerHTML = tableHTML;
 }
 
-inputBtn.addEventListener("click", function () {
+inputBtn.addEventListener("click", () => {
   // making sure the input is valid and the user isn't passing an empty string
   if (name.value != "" && dateEl.value != "" && amountEl.value != "") {
     myTable.push([name.value, dateEl.value, amountEl.value]);
@@ -53,5 +56,8 @@ inputBtn.addEventListener("click", function () {
     amountEl.value = "";
     localStorage.setItem("myTable", JSON.stringify(myTable));
     render(myTable);
+  } else {
+    message.textContent = "Please Fill In All Input Fields";
+    setTimeout(() => message.remove(), 3000);
   }
 });
